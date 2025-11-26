cabd432b2b9b10a6481b31c17b812436ceaf7492
Pawantripathi2606 <pawantripathi802@gmail.com>
2025-11-26 17:02:27 +0530

    Initial commit

---
+++ src/Pattern.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Pattern {
+    static void main(String[] args) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+
+    }
+}
