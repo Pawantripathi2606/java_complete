8e29b5fa373be793b5ae94d9c2878726216d0d5b
Pawantripathi2606 <pawantripathi802@gmail.com>
2025-11-26 20:22:41 +0530

    Initial commit

--- src/Variables.java
+++ src/Variables.java
@@ -7,6 +7,29 @@ public class Variables {
        mynum=1234;
        System.out.println("this is your number " + mynum);
        System.out.println(String.format("this is your number %d" , mynum));
-       System.out.printf("this is your number %d " , mynum);
+       System.out.printf("this is your number %d\n " , mynum);
+       yournum= 1010101;
+       System.out.println("this is your previously defined variable value "+yournum);
+
+        // the direct value we use in program is called literals
+       // literals are assigned after veriables
+
+       // datatype identifiers = literals
+
+
+       boolean isveg = false ;
+       System.out.println(isveg);
+
+       float num=5.0f;
+       System.out.println(num);
+
+       float num2=5;
+       System.out.println(num);
+
+       double mydouble = 456;
+       System.out.println(mydouble);
+
+       String wishes = "heyyy congo[";
+       System.out.println(wishes);
     }
 }
