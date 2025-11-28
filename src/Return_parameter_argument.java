620332fa5e3a49485e52eaa42a9225d6dacb84dd
Pawantripathi2606 <pawantripathi802@gmail.com>
2025-11-29 03:24:20 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/Return_parameter_argument.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Return_parameter_argument {
+    static void main(String[] args) {
+
+        greet();
+        int first = readnumber();
+        int second =   readnumber();
+        int sum = add(first,second); //arguments -- passing values
+        System.out.println("your addition is : "+sum);
+
+    }
+    public static void greet(){
+        System.out.println("welcome to calculator");
+
+    }
+
+    public static int readnumber(){  //using datatype to return
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your number : ");
+        int number = input.nextInt();
+        return number; // return
+    }
+
+    public static int add(int first , int second){  // paramters   -- defining
+        return first+second;
+    }
+}
